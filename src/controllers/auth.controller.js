5b44b82fd7c70fd40a1dd9ca42ae9021a6bd684a
J <133668286+alswla@users.noreply.github.com>
2024-03-24 06:09:43 +0900

    Feat: create asana auth routers and controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,23 +1,26 @@
-const { getOutlookAuthUrl } = require("../services/getOutlookAuth");
+const crypto = require("crypto");
+const { URL, URLSearchParams } = require("url");
+
 const { getGoogleAuthUrl } = require("../services/getGoogleAuth");
 
 exports.googleAuthHandler = async (req, res, next) => {
   const googleAuthUrl = getGoogleAuthUrl();
 
-  // res.redirect(googleAuthUrl + "&state=google");
-
   res.redirect(googleAuthUrl);
 };
 
-exports.outlookAuthHandler = async (req, res, next) => {
-  const outlookAuthUrl = await getOutlookAuthUrl();
+exports.asanaAuthHandler = async (req, res, next) => {
+  const asanaAuthBaseUrl = new URL("https://app.asana.com/-/oauth_authorize");
 
-  // TODO. 배포 이후 HTTPS 사용시에는 해당 주석을 활성화합니다.
-  //       주석 활성화 이후 SameSite쿠키 활성화를 통해 ThirdParty Cookie 문제를 해결합니다.
-  //       이 경우에는 App의 타입을 SPA로 변경하고, 클라이언트 요청 로직을 변경해야 합니다.
-  // res.cookie("name", "value", { sameSite: "none", secure: true });
-  // res.status(200).json({ url: outlookAuthUrl });
+  const params = new URLSearchParams({
+    client_id: process.env.ASANA_CLIENT_ID,
+    redirect_uri: process.env.AUTH_REDIRECT_URI_ASANA,
+    response_type: "code",
+    state: process.env.ASANA_STATE_SECRET,
+  });
 
-  // res.redirect(outlookAuthUrl + "&state=outlook");
-  res.redirect(outlookAuthUrl);
+  asanaAuthBaseUrl.search = params.toString();
+  console.log(asanaAuthBaseUrl.toString());
+
+  res.redirect(asanaAuthBaseUrl.toString());
 };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,10 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/auth.controller");
+const userController = require("../controllers/user.controller");
 
 router.get("/google", authController.googleAuthHandler);
-router.get("/outlook/proxy", authController.outlookAuthHandler);
-/* TODO: 배포 이후 HTTPS 사용시에는 해당 주석을 활성화합니다.
-router.post("/outlook", authController.outlookAuthHandler); */
+
+router.get("/asana", authController.asanaAuthHandler);
+router.get("/asana/users", userController.getAsanaUser);
 
 module.exports = router;
