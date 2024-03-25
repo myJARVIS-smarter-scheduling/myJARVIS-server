4c53f9703474fe490a1a4c1bc64610ce5d84d619
J <133668286+alswla@users.noreply.github.com>
2024-03-26 02:01:48 +0900

    Feat: create fetch and save mircosoft calendar features

--- src/controllers/calendar.controller.js
+++ src/controllers/calendar.controller.js
@@ -1,13 +1,10 @@
 const { googleLogin } = require("../services/getGoogleAuth");
-const {
-  getGoogleUserInfo,
-  getOutlookUserInfo,
-} = require("../services/getUserInfo");
-const {
-  getGoogleCalendarEvents,
-  getOutlookCalendarEvents,
-} = require("../services/getCalendarEvents");
+const { getGoogleUserInfo } = require("../services/getUserInfo");
+const { getGoogleCalendarEvents } = require("../services/getCalendarEvents");
 const { saveCalendarEvents } = require("../services/saveCalendarEvents");
+const {
+  setupAllCalendarSubscriptions,
+} = require("../services/subscribeGoogleWebhook");
 const { User, Account } = require("../models/User");
 const { Event } = require("../models/Event");
 
@@ -99,6 +96,8 @@ exports.saveGoogleUserAndCalendar = async (req, res, next) => {
 
     await user.save();
 
+    await setupAllCalendarSubscriptions(account);
+
     if (!userId) {
       res.cookie(
         "accessToken",
@@ -126,6 +125,110 @@ exports.saveGoogleUserAndCalendar = async (req, res, next) => {
   } catch (error) {
     console.error("google calendar controller error:", error);
   }
+};
+
+exports.saveOutlookUserAndCalendar = async (req, res, next) => {
+  const { userId } = req.cookies;
+  const { accountsData } = req.body;
+  const { userInfo, mailboxSettings, calendarEvents, accessToken } =
+    accountsData[accountsData.length - 1];
+  const email = userInfo.mail;
+
+  let user = await User.findOne({ email });
+  const loginUser = await User.findById(userId);
+  const provider = "microsoft";
+
+  if (!user && !userId) {
+    user = new User({
+      email,
+      name: userInfo.displayName,
+      timezone: mailboxSettings.timeZone,
+      language: mailboxSettings.locale,
+      provider,
+      accessToken,
+      // TODO: 추후 channelId, resourceId 추가
+    });
+
+    await user.save();
+  } else if (!userId && user) {
+    user.accessToken = accessToken;
+
+    await user.save();
+  } else {
+    user = loginUser;
+  }
+
+  let account = await Account.findOne({
+    userId: user._id,
+    email,
+    provider,
+  });
+
+  if (!account) {
+    account = new Account({
+      userId: user._id,
+      provider,
+      email,
+      accessToken,
+    });
+
+    await account.save();
+
+    user.accountList.push(account);
+  } else {
+    account.accessToken = accessToken;
+
+    await account.save();
+  }
+
+  if (calendarEvents.value.length > 0) {
+    await saveCalendarEvents(
+      account._id,
+      calendarEvents.value,
+      account.provider,
+      user.timezone,
+    );
+  }
+
+  await user.save();
+
+  if (!userId) {
+    res.cookie(
+      "accessToken",
+      user.accessToken,
+      {},
+      {
+        httpOnly: true,
+        secure: true,
+        expires: 60 * 60 * 24 * 7 * 1000,
+      },
+    );
+    res.cookie(
+      "userId",
+      user._id.toString(),
+      {},
+      {
+        httpOnly: true,
+        secure: true,
+        expires: 60 * 60 * 24 * 7 * 1000,
+      },
+    );
+  }
+
+  const eventPromiseList = user.accountList.map(async (accountdata) => {
+    const events = await Event.find({ accountId: accountdata._id });
+
+    return { accountId: accountdata._id, email: accountdata.email, events };
+  });
+
+  const accountEventList = await Promise.all(eventPromiseList);
+  const accountInfo = { email: user.email, timezone: user.timezone };
+
+  return res.status(201).json({
+    result: "success",
+    accountEventList,
+    user: accountInfo,
+  });
 };
 
 exports.transferCalendarEvents = async (req, res, next) => {
--- src/routes/calendar.js
+++ src/routes/calendar.js
@@ -7,6 +7,7 @@ const calendarController = require("../controllers/calendar.controller");
 
 router.get("/google", calendarController.saveGoogleUserAndCalendar);
 
+router.post("/outlook", calendarController.saveOutlookUserAndCalendar);
 router.post("/events", verifyToken, calendarController.transferCalendarEvents);
 
 module.exports = router;
